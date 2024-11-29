5b0eab96ab702777b120e7ef9cd8f24b5689f0e6
mlbd <mlimonbd@gmail.com>
2024-11-30 04:07:13 +0600

    fix veriables

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 // api/index.js
 
 module.exports = (req, res) => {
-  res.status(200).send("404: Lost route—probably chasing squirrels. ");
+  const REDIRECT_URI = process.env.REDIRECT_URI;
+  res
+    .status(200)
+    .send(
+      "404: Lost route—probably chasing squirrels. redirect_uri: " +
+        REDIRECT_URI
+    );
 };
